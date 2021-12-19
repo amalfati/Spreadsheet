a582f5814e61cbde223236f9604d7be10be2823a
amalfati <93687959+amalfati@users.noreply.github.com>
2021-12-19 13:50:53 +0100

    Update dialog1.cpp

--- dialog1.cpp
+++ dialog1.cpp
@@ -6,8 +6,7 @@ Dialog1::Dialog1(QWidget *parent) :
     ui(new Ui::Dialog1)
 {
     ui->setupUi(this);
-//    QRegExp exp {"[A-Z][1-9][0-9]{0,2}"};
-//    ui->lineEdit->setValidator(new QRegExpValidator(exp));
+
 }
 
 Dialog1::~Dialog1()
